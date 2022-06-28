0ef5d589747c882b2776134ddea08536a98b3a54
blam <betsyrlam@gmail.com>
2022-06-28 09:36:31 -0500

    enable admin changes in stg,prod

--- config/general.php
+++ config/general.php
@@ -30,7 +30,7 @@ return [
     'devMode' => $isDev,
 
     // Whether administrative changes should be allowed
-    'allowAdminChanges' => $isDev,
+    'allowAdminChanges' => true,//$isDev,
 
     // Whether crawlers should be allowed to index pages and following links
     'disallowRobots' => !$isProd
